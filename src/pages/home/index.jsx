3c755ad4b890c368b506015735442638b8d4bd65
Decronus <zick3333@mail.ru>
2023-03-10 01:45:58 +0100

    init commit

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import Adv from "../../components/adv";
+import { StyledContainer } from "../../global-styles";
+import { get } from "../../utils/fetch";
+import * as S from "./styles";
+
+function Home() {
+  const [adsList, setAdsList] = useState([]);
+
+  useEffect(() => {
+    getAdv();
+  }, []);
+
+  const getAdv = async () => {
+    const { json } = await get("/ads");
+    setAdsList(json);
+  };
+
+  return (
+    <S.Main>
+      <StyledContainer>
+        <S.Title>Объявления</S.Title>
+        <S.AdvList>
+          {adsList.map((adv) => (
+            <Adv key={adv.id} adv={adv}/>
+          ))}
+        </S.AdvList>
+      </StyledContainer>
+    </S.Main>
+  );
+}
+
+export default Home;
